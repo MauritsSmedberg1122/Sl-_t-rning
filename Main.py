7318f6f85421d4411f6ed5edb3d64bb3c3413267
Maurits <maurits.smedberg@elev.ga.ntig.se>
2022-05-04 14:53:10 +0200

    Today i maked the code easyier to read and used new imports

--- Main.py
+++ Main.py
@@ -1,19 +1,34 @@
 # ------------------ BEGIN: Import --------------------
 import random
+from time import sleep
+import sys
+import os
 # ------------------ END: Import ----------------------
 
 # ------------------ BEGIN: Constants and global variables ---------------
 # ------------------ END: Constants and global variables -----------------
 
 # ------------------ BEGIN: Functions ------------------------------------
+words=""
+def speech():
+    for char in words:
+        sleep(0.05)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+
+    print(os.linesep)
 # ------------------ END: Functions --------------------------------------
 
 # ------------------ BEGIN: Main function ------------------------------------
-def main ():
+def main():
+    global words
     # Tell the user the instructions about the game
-    print ("Welcome to roll a dice!")
-    print ("")
-
+    words = "Welcome to roll a dice!"
+    speech() 
+    sleep(1)
+    words = f"In this game, your task is to first roll the dice. Then, with the help of mathematics, you should get the sum to 25.{os.linesep}Good luck!"
+    speech()
+    sleep(1.5)
     # Throw the blue and red dice
     red_dice = random.randrange (1, 7)
     blue_dice = random.randrange (1, 7)
@@ -28,9 +43,14 @@ def main ():
         white_dices.append(white_dice)
 
     # Show the user the dices
-    print ("I roll the dices")
-    print ("Blue dice:", blue_dice)
-    print ("Red dice:", red_dice)
+    words = "I roll the dices"
+    speech() 
+    sleep(1)
+    words = f"Blue dice: {blue_dice}"
+    speech() 
+    sleep(0.5)
+    words = f"Red dice: {red_dice}"
+    speech() 
 # ------------------ END: Main function --------------------------------------
 
 # ------------------ BEGIN: Entrance path ------------------------------------
