eebfccdb76f7a367892d4f7348d1e231da92506a
Maurits <maurits.smedberg@elev.ga.ntig.se>
2022-05-07 15:59:19 +0200

    Finished the calculation function and the main function

--- Main.py
+++ Main.py
@@ -1,34 +1,47 @@
 # ------------------ BEGIN: Import --------------------
 import random
 from time import sleep
-import sys
-import os
 # ------------------ END: Import ----------------------
 
 # ------------------ BEGIN: Constants and global variables ---------------
 # ------------------ END: Constants and global variables -----------------
 
 # ------------------ BEGIN: Functions ------------------------------------
-words=""
-def speech():
+def speech(words, time_in_seconds):
     for char in words:
         sleep(0.05)
-        sys.stdout.write(char)
-        sys.stdout.flush()
+        print(char, end="", flush=True)
 
-    print(os.linesep)
+    print()
+    sleep(time_in_seconds)
+
+def calcution(user_calculation, dices):
+    # user_calculation = "1+2*3-4+5"
+    # dices = [1,2,3,4,5]
+
+    # Gå igenom varje bokstav/siffra i texten och kontrollera vad du ska 
+    
+    # I slutet vill vi ha ett tal som är:
+    # goal = 1+2*3-4+5 = 10
+
+    # Look through all characters in the users calculation.
+    goal = 0
+    for letter in user_calculation:
+        # Control if the letter is a number
+        if letter.isdigit():
+            pass
+    return goal
+
 # ------------------ END: Functions --------------------------------------
 
 # ------------------ BEGIN: Main function ------------------------------------
 def main():
-    global words
     # Tell the user the instructions about the game
-    words = "Welcome to roll a dice!"
-    speech() 
-    sleep(1)
-    words = f"In this game, your task is to first roll the dice. Then, with the help of mathematics, you should get the sum to 25.{os.linesep}Good luck!"
-    speech()
-    sleep(1.5)
+    speech("Welcome to roll a dice!", 1) 
+
+    words = f"In this game, your task is to first roll the dice. Then, with the help of mathematics, you should get the result to a number.\nGood luck!"
+    speech(words, 1.5)
+
     # Throw the blue and red dice
     red_dice = random.randrange (1, 7)
     blue_dice = random.randrange (1, 7)
@@ -43,14 +56,15 @@ def main():
         white_dices.append(white_dice)
 
     # Show the user the dices
-    words = "I roll the dices"
-    speech() 
-    sleep(1)
-    words = f"Blue dice: {blue_dice}"
-    speech() 
-    sleep(0.5)
-    words = f"Red dice: {red_dice}"
-    speech() 
+    speech("I roll the dices", 1) 
+    speech(f"Blue dice: {blue_dice}", 0.5) 
+    speech(f"Red dice: {red_dice}", 0.5) 
+    speech(f"White dices: {white_dices}", 2)
+
+    speech(f"To win you should get the result: {goal}", 1)
+
+    # Now it is time for the user to input his guess.
+    # When we have the input call the calculation function.
 # ------------------ END: Main function --------------------------------------
 
 # ------------------ BEGIN: Entrance path ------------------------------------
